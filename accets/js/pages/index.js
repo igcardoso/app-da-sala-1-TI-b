87c403d024684e4bdafddba45793b04a13cc7a6b
igor <85964276+igcardoso@users.noreply.github.com>
2022-12-28 19:06:16 -0300

    Delete accets directory

--- accets/js/pages/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('execCopyCodigoAdm').addEventListener('click', execCopyCodigoAdm);
-function execCopyCodigoAdm() {
-  document.querySelector("#inputCopyCodigoAdm").select();
-  document.execCommand("copy");
-  window.alert("link copiado");
-}

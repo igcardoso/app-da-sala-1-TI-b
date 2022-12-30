5774f9e40c8c926338955025140240c2758a65b8
igor <85964276+igcardoso@users.noreply.github.com>
2022-12-30 08:50:19 -0300

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,18 @@
-const staticDevCoffee = "infinix-v1";
+const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
   "/",
   "/index.html",
   "/css/style.css",
   "/js/app.js",
-  "/images/20221224203650.png",
-  "/images/app-store.png",
-  "/images/google-play.png",
-  "/images/icon-multiple.png",
-  "/images/IMG-20220930-WA0047.jpg",
-  "/images/IMG-20221123-WA0048.jpg",
-  "/images/IMG_20221223_182141.jpg",
-  "/images/IMG_20221224_102948~2.jpg",
-  "/images/IMG_20221224_171140.jpg",
-  "/images/IMG_20221224_171224.jpg",
-  "/images/IMG_20221224_182958.jpg",
-  "/images/Logotipo_IFET.svg.png"
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg"
 ];
 
 self.addEventListener("install", installEvent => {

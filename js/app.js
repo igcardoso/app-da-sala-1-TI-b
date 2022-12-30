a003a2d0bb7f9e5909725e8200df8798ad943e45
igor <85964276+igcardoso@users.noreply.github.com>
2022-12-30 14:06:03 -0300

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,35 +1,3 @@
-/*  ********** Online / Offline Detection **********  */
-
-// Request a small image at an interval to determine status
-// ** Get a 1x1 pixel image here: http://www.1x1px.me/
-// ** Use this code with an HTML element with id="status"
-
-const checkOnlineStatus = async () => {
-  try {
-    const online = await fetch("/1pixel.png");
-    return online.status >= 200 && online.status < 300; // either true or false
-  } catch (err) {
-    return false; // definitely offline
-  }
-};
-
-setInterval(async () => {
-  const result = await checkOnlineStatus();
-  const statusDisplay = document.getElementById("status");
-  statusDisplay.textContent = result ? "Online" : "OFFline";
-}, 3000); // probably too often, try 30000 for every 30 seconds
-
-// forgot to include async load event listener in the video! 
-window.addEventListener("load", async (event) => {
-  const statusDisplay = document.getElementById("status");
-  statusDisplay.textContent = (await checkOnlineStatus())
-    ? "Online"
-    : "OFFline";
-});
-
-if (status == "OFFline") {
-    window.location.href = "http://www.devmedia.com.br";
-}
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {

18f7a9e722f77548ff640a8f2333f68055cad0b9
igor <85964276+igcardoso@users.noreply.github.com>
2022-12-30 21:21:22 -0300

    update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,28 @@ if ("serviceWorker" in navigator) {
   });
 }
 
-let pag1 = document.querySelector('.containment-of-menus .op1')
+let pag1 = document.querySelector('.containment-of-menus .op1');
 
-let pag2 = document.querySelector('.containment-of-menus .op2')
+let pag2 = document.querySelector('.containment-of-menus .op2');
+
+let pag3 = document.querySelector('.containment-of-menus .op3');
+
+let pag4 = document.querySelector('.containment-of-menus .op4');
+
+let pag5 = document.querySelector('.containment-of-menus .op5');
+
+let pag6 = document.querySelector('.containment-of-menus .op6');
+
+let pag7 = document.querySelector('.containment-of-menus .op7');
+
+pag1.addEventListener('click', select);
+pag2.addEventListener('click', select);
+pag3.addEventListener('click', select);
+pag4.addEventListener('click', select);
+pag5.addEventListener('click', select);
+pag6.addEventListener('click', select);
+pag7.addEventListener('click', select);
+
+function select() {
+  console.log('funciona 1');
+}

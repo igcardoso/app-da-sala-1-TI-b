f0c18fc05f6bceb1b489fc965e823258e86c3bc0
igor <85964276+igcardoso@users.noreply.github.com>
2023-01-01 17:49:18 -0300

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,15 +6,18 @@ const assets = [
   "/css/style.css",
   "/js/app.js",
   "/manifest.json",
-  "/images/coffee1.jpg",
-  "/images/coffee2.jpg",
-  "/images/coffee3.jpg",
-  "/images/coffee4.jpg",
-  "/images/coffee5.jpg",
-  "/images/coffee6.jpg",
-  "/images/coffee7.jpg",
-  "/images/coffee8.jpg",
-  "/images/coffee9.jpg"
+  "/images/12 Sem Título_20230101152253.png",
+  "/images/20221224203650.png",
+  "/images/IMG-20220930-WA0047.jpg",
+  "/images/IMG-20221123-WA0048.jpg",
+  "/images/IMG_20221223_182141.jpg",
+  "/images/IMG_20221224_102948~2.jpg",
+  "/images/IMG_20221224_171140.jpg",
+  "/images/IMG_20221224_171224.jpg",
+  "/images/IMG_20221224_182958.jpg",
+  "/images/Logotipo_IFET.svg.png",
+  "/images/app-store.png",
+  "/images/google-play.png"
 ];
 
 self.addEventListener("install", installEvent => {
